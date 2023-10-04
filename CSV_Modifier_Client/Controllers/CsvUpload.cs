9cc4b7447258ded0e385bdc9db2e3d4ab972ca76
Sikandar <sikandar.ramzan@codeninjaconsulting.com>
2023-10-04 09:07:06 +0500

    sns-sqn

--- CSV_Modifier_Client/Controllers/CsvUpload.cs
+++ CSV_Modifier_Client/Controllers/CsvUpload.cs
@@ -4,13 +4,18 @@ using CSV_Modifier_Client.Models;
 using Microsoft.AspNetCore.Mvc;
 using CSV_Modifier_Client.Services;
 using CSV_Modifier_Client.Core;
+using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace CSV_Modifier_Client.Controllers
 {
     public class CsvUpload : Controller
     {
-        private readonly string bucketName = Constants.BucketName;
         private readonly AwsSecretsService _awsSecretsService;
+
         public CsvUpload(AwsSecretsService awsSecretsService)
         {
             _awsSecretsService = awsSecretsService;
@@ -30,31 +35,45 @@ namespace CSV_Modifier_Client.Controllers
                 ModelState.AddModelError("csvFile", "Please select a CSV file.");
                 return View("Index");
             }
-            string superman_access_key = await _awsSecretsService.GetAwsSecret("superman_access_key");
-            string superman_secret_superman_access_key = await _awsSecretsService.GetAwsSecret("superman_secret_access_key");
+            string superman_access_key = await _awsSecretsService.GetAwsSecret(Constants.SuperUserAccessKey);
+            string superman_secret_access_key = await _awsSecretsService.GetAwsSecret(Constants.SuperUserSectretKey);
             try
             {
-                using var s3Client = new AmazonS3Client(superman_access_key, superman_secret_superman_access_key);
+                using var s3Client = new AmazonS3Client(superman_access_key, superman_secret_access_key);
 
                 using var memoryStream = new MemoryStream();
                 await csvFile.CopyToAsync(memoryStream);
                 memoryStream.Seek(0, SeekOrigin.Begin);
 
                 var fileTransferUtility = new TransferUtility(s3Client);
-                await fileTransferUtility.UploadAsync(memoryStream, bucketName, csvFile.FileName);
+                await fileTransferUtility.UploadAsync(memoryStream, Constants.BucketName, csvFile.FileName);
 
-                var model = new UploadViewModel
+                var credentials = new BasicAWSCredentials(superman_access_key, superman_secret_access_key);
+                var client = new AmazonSimpleNotificationServiceClient(credentials, Amazon.RegionEndpoint.USEast1);
+                var request = new PublishRequest
                 {
-                    Message = "CSV file uploaded successfully to S3"
+                    TopicArn = Constants.CsvModifierTopicArn,
+                    Message = csvFile.FileName,
+                    Subject = Constants.BucketName,
                 };
 
-                return View("Index", model);
+               await client.PublishAsync(request);
+
             }
             catch (Exception ex)
             {
                 ModelState.AddModelError("csvFile", $"Error: {ex.Message}");
                 return View("Index");
             }
+
+           
+
+            var model = new UploadViewModel
+            {
+                Message = "CSV file uploaded successfully to S3"
+            };
+
+            return View("Index", model);
         }
     }
 }
